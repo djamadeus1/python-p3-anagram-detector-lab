4e069841d1033e0aae99e68c02aa543707436a72
Ronald Roberts <djamadeus1@gmail.com>
2024-11-22 15:01:24 -0500

    whatever

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram: # your code goes here!
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        matches = [word for word in possible_anagrams if sorted(word) == sorted_word]
+        return matches
+    
